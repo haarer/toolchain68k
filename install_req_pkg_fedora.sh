72e4d329e0eaffb4544d541b6e6ff07070c527ca
haarer <alexander.haarer@gmail.com>
2017-05-23 10:00:51 +0200

    work on package installation scripts, they are used to pull in the build platform specific dependencies needed for the built of the toolchain

--- install_req_pkg_fedora.sh
+++ install_req_pkg_fedora.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # install fedora package requirements, based on a minimum install 
 #( install sudo first and add your user to the sudoers)
-sudo yum install redhat-lsb-core git make tar flex bison diffutils texinfo gcc wget bzip2
+sudo yum install redhat-lsb-core git make tar flex bison diffutils texinfo gcc  gcc-c++ wget bzip2 libstdc++-devel
