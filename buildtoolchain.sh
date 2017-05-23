53b7213a91ff1dee59435de30ba4681e72f22c40
haarer <alexander.haarer@gmail.com>
2017-05-23 09:56:45 +0200

    beginning support of linux build platforms (fedora,debian) added avr support (uses avr-libc instead of newlib)

--- buildtoolchain.sh
+++ buildtoolchain.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright (c) 2014, A.Haarer, All rights reserved. LGPLv3
 
 # build 68k cross toolchain based on gcc
 # set the number of make jobs as appropriate for build machine
 # set the path after installation : export PATH=/opt/m68k/bin:$PATH
 
-# first version gcc 5 (tested on debian wheezy, may 2015)
-# in may 2017 reworked using MSYS2
+# tested on host platforms : msys2 64bit on windows 10, debian 8
+
 # links:
 #   http://www.mikrocontroller.net/articles/GCC_M68k
 #   https://gcc.gnu.org/install/index.html
@@ -14,25 +14,45 @@
 #   https://gcc.gnu.org/wiki/FAQ#configure
 #   http://www.msys2.org/
 
-# MSYS2 package requirements
-# flex bison diffutils texinfo make
-# mingw-w64-x86_64-gcc  
-# mingw-w64-x86_64-libmangle-git 
-# mingw-w64-x86_64-make
-# mingw-w64-x86_64-pkg-config
-# mingw-w64-x86_64-tools-git
-# mingw-w64-x86_64-winstorecompat-git
 
-
-
+# Tips:
 # to speed up things:  chose a location without 
-# - indexing (dont use windows home dir or a
-# - virus scanner ( add exclusion)
+# - indexing (dont use windows home dir )
+# - virus scanner ( add exclusion, or stop scanner)
+# 
+# chose a number of make jobs #number of cores + 1 to make use of all computer resources
 #
+# some sh versions dont like the () after functions - call the script using bash helps
+#
+# on linux the script asks for su privileges at start - they are kept during script runtime but used only for make install
+
 LOGFILE="`pwd`/buildlog.txt"
 #set the number of parallel makes
-MAKEJOBS=4
+MAKEJOBS=5
+TARGETARCHITECTURE=avr
+#TARGETARCHITECTURE=m68k-elf
 
+export PATH=/opt/m68k/bin:$PATH
+
+function determine_os () {
+if [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release 
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /usr/bin/lsb_release ]; then
+    OS=`/usr/bin/lsb_release -is`
+    VER=`/usr/bin/lsb_release -rs`
+elif [ -f /etc/debian_version ]; then
+    OS=Debian  # XXX or Ubuntu??
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/redhat-release ]; then
+    # TODO add code for Red Hat and CentOS here
+    ...
+else
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+}
 
 function log_msg () {
     local logline="`date` $1"
@@ -83,31 +103,59 @@ function prepare_source () {
 
 }
 
+#function to install - requests privileges from user if os requires
+# issues: 
+# *  is the detection safe ?
+#   
+function install_package () {
+    if [ $OS = "Debian" ] [ $OS = "Fedora" ]; then
+        sudo sh -c "export PATH=$PATH:/opt/m68k/bin; make install"
+    else
+        make install
+    fi
+}
+
 function conf_compile_source () {
     local SOURCEPACKAGE=$1
     local DETECTFILE=$2
     local CONFIGURESTRING=$3
 
-    if [ ! -f $DETECTFILE ]; then
+    if [ ! -f config.status ]; then
         log_msg "configuring $SOURCEPACKAGE"
-        ../configure $CONFIGURESTRING
+        ../configure $CONFIGURESTRING || exit 1
         log_msg "configuring $SOURCEPACKAGE finished"
+    else
+        log_msg "configuring $SOURCEPACKAGE skipped"
+    fi
+
+    if [ ! -f $DETECTFILE ]; then
 
         log_msg "building $SOURCEPACKAGE"
         make -j $MAKEJOBS
         log_msg "building $SOURCEPACKAGE finished"
 
         log_msg "install $SOURCEPACKAGE"
-        make install
+        install_package
         log_msg "install $SOURCEPACKAGE finished"
     else
-        log_msg "configuring, compiling and install $SOURCEPACKAGE skipped"
+        log_msg "compiling and install $SOURCEPACKAGE skipped"
     fi
 
 }
 
-
+# -------------------------------------------------------------------------------------------
 log_msg " start of buildscript"
+
+determine_os
+log_msg " building on OS: $OS $VER for target architecture $TARGETARCHITECTURE"
+if [ "$OS" = "MINGW64_NT-10.0" ]; then
+	EXECUTEABLESUFFIX=".exe"
+	echo "ouuch.. on windows"
+else
+	echo "eeek.. not on windows, doing sudo keepalive tricks"
+	sudo -v
+	while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+fi
 
 if [ ! -d cross-m68k ]; then
 	mkdir cross-m68k
@@ -118,7 +166,7 @@ cd cross-m68k
 M68KBUILD=`pwd`
 echo "build path:" $M68KBUILD
 
-#export PATH=/opt/m68k/bin:$PATH
+
 
 #----------------------------------------------------------------------------------
 
@@ -127,7 +175,7 @@ BINUTILS="binutils-2.25"
 
 prepare_source http://ftp.gnu.org/gnu/binutils  $BINUTILS tar.bz2
 
-conf_compile_source binutils "/opt/m68k/bin/m68k-elf-objcopy.exe" "--target=m68k-elf --prefix=/opt/m68k/" 
+conf_compile_source binutils "/opt/m68k/bin/$TARGETARCHITECTURE-objcopy$EXECUTEABLESUFFIX" "--target=$TARGETARCHITECTURE --prefix=/opt/m68k/" 
 
 cd $M68KBUILD
 
@@ -146,24 +194,42 @@ if [ ! -d ../gmp ]; then
     cd m68k-obj
 fi
 
-conf_compile_source gcc "/opt/m68k/bin/m68k-elf-gcov.exe" "--target=m68k-elf --prefix=/opt/m68k/ --enable-languages=c,c++ --disable-bootstrap --with-newlib --disable-libmudflap --disable-libssp --disable-libgomp --disable-libstdcxx-pch --disable-threads --with-gnu-as --with-gnu-ld --disable-nls --with-headers=yes --disable-checking --without-headers"
+conf_compile_source gcc "/opt/m68k/bin/$TARGETARCHITECTURE-gcov$EXECUTEABLESUFFIX" "--target=$TARGETARCHITECTURE --prefix=/opt/m68k/ --enable-languages=c,c++ --disable-bootstrap --with-newlib --disable-libmudflap --disable-libssp --disable-libgomp --disable-libstdcxx-pch --disable-threads --with-gnu-as --with-gnu-ld --disable-nls --with-headers=yes --disable-checking --without-headers"
 
 cd $M68KBUILD
 
-#---------------------------------------------------------------------------------
-#build libc
+if [ "$TARGETARCHITECTURE" = "avr" ]; then
+    #---------------------------------------------------------------------------------
+    #build avr libc
 
-log_msg ">>>> build newlib"
-LIBCVER="newlib-2.5.0"
+    log_msg ">>>> build avrlib"
+    LIBCVER="avr-libc-2.0.0"
 
-prepare_source ftp://sources.redhat.com/pub/newlib $LIBCVER tar.gz
+    prepare_source http://download.savannah.gnu.org/releases/avr-libc $LIBCVER tar.bz2
 
-export PATH=$PATH:/opt/m68k/bin/
+    export PATH=$PATH:/opt/$TARGETARCHITECTURE/bin/
 
-conf_compile_source newlib "/opt/m68k/m68k-elf/lib/mfidoa/softfp/libc.a" " --target=m68k-elf --prefix=/opt/m68k/ --enable-newlib-reent-small --disable-malloc-debugging --enable-newlib-multithread --disable-newlib-io-float --disable-newlib-supplied-syscalls --disable-newlib-io-c99-formats --disable-newlib-mb --disable-newlib-atexit-alloc --enable-target-optspace --disable-shared --enable-static --enable-fast-install"
+    conf_compile_source avrlib "/opt/m68k/$TARGETARCHITECTURE/lib/libc.a" "--host=avr --prefix=/opt/m68k/"
 
-cd $M68KBUILD
+    cd $M68KBUILD
+
+else
+
+    #---------------------------------------------------------------------------------
+    #build libc for other platforms
+
+    log_msg ">>>> build newlib"
+    LIBCVER="newlib-2.5.0"
+
+    prepare_source ftp://sources.redhat.com/pub/newlib $LIBCVER tar.gz
+
+    export PATH=$PATH:/opt/$TARGETARCHITECTURE/bin/
+
+    conf_compile_source newlib "/opt/m68k/$TARGETARCHITECTURE/lib/mfidoa/softfp/libc.a" " --target=$TARGETARCHITECTURE --prefix=/opt/m68k/ --enable-newlib-reent-small --disable-malloc-debugging --enable-newlib-multithread --disable-newlib-io-float --disable-newlib-supplied-syscalls --disable-newlib-io-c99-formats --disable-newlib-mb --disable-newlib-atexit-alloc --enable-target-optspace --disable-shared --enable-static --enable-fast-install"
+
+    cd $M68KBUILD
  
+fi 
 
 #erst mal ohne gdb
 
@@ -175,10 +241,10 @@ GDBVER="gdb-7.12"
 log_msg ">>>> build gdb"
 prepare_source http://ftp.gnu.org/gnu/gdb $GDBVER tar.xz
 
-conf_compile_source gdb "/opt/m68k/bin/m68k-elf-gdb.exe" "--target=m68k-elf --prefix=/opt/m68k/"
+conf_compile_source gdb "/opt/m68k/bin/$TARGETARCHITECTURE-gdb$EXECUTEABLESUFFIX" "--target=$TARGETARCHITECTURE --prefix=/opt/m68k/"
 
 cd $M68KBUILD
-exit
+
 
 # musasim bauen
 # https://code.google.com/p/musasim/
