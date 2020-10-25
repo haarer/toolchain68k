918aa0242f5004c5fe014df2b7b5ca6ad9925426
mac <haarer@github.com>
2020-10-25 13:46:31 +0100

    add unzip

--- install_req_pkg_ubuntu-latest.sh
+++ install_req_pkg_ubuntu-latest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # install debian package requirements , based on a minimum install
 # add your user to the sudoers
-sudo apt-get install  wget bzip2 git make tar flex bison diffutils texinfo gcc g++ libstdc++-8-dev
+sudo apt-get install  wget bzip2 unzip git make tar flex bison diffutils texinfo gcc g++ libstdc++-8-dev
