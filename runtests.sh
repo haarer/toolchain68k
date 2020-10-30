fb1f1e9450b18abde8c3138d5a6c19a86fa72916
mac <haarer@github.com>
2020-10-30 23:17:36 +0100

    get stdperiph lib from own repo

--- runtests.sh
+++ runtests.sh
@@ -5,6 +5,7 @@
 function test_build_result () {
   local TESTFILE=$1
   local NOCOLOR='\033[0m'
+  local GREEN='\033[0;32m'
   local RED='\033[0;31m'
  if [ ! -f $TESTFILE ]; then
    echo -e "${RED}Build Error${NOCOLOR} : missing file $TESTFILE"
@@ -20,27 +21,37 @@ echo $PATH
 
 case $TARGETARCHITECTURE in
 "arm-none-eabi")
-     ( cd examples/arm-none-eabi-example && \
-       wget -nc https://www.st.com/content/ccc/resource/technical/software/firmware/48/ab/e5/17/0d/79/43/74/stsw-stm32054.zip/files/stsw-stm32054.zip/_jcr_content/translations/en.stsw-stm32054.zip &&\
-       [ -d "STM32F10x_StdPeriph_Lib_V3.5.0/Libraries" ] || unzip en.stsw-stm32054.zip &&\
-       make clean && make arm-test.bin )
+    [ -d "binary_assets" ] || git clone https://github.com/haarer/binary_assets.git
+    ( cd examples && [ -d "STM32F10x_StdPeriph_Lib_V3.5.0/Libraries" ] || unzip ../binary_assets/en.stsw-stm32054.zip )
+    ( cd examples/arm-none-eabi-example ; make clean && make arm-test.bin )
     test_build_result "examples/arm-none-eabi-example/arm-test.bin" 
+    ( cd examples/arm-none-eabi-example ; make clean )
   ;;
 "m68k-elf")
     ( cd examples/m68k-example && make clean && make files )
     test_build_result "examples/m68k-example/m68k-test.bin" 
+    ( cd examples/m68k-example && make clean )
+    
     ( cd examples/m68k-example2 && make clean && make files )
     test_build_result "examples/m68k-example2/m68k-test.bin" 
+    ( cd examples/m68k-example2 && make clean)
   ;;
 "avr")
     ( cd examples/avr-example && make clean && make avr-test.hex )
     test_build_result "examples/avr-example/avr-test.hex" 
+    ( cd examples/avr-example && make clean )
+
     ( cd examples/avr-example2 && make clean && make avr-test.hex )
     test_build_result "examples/avr-example2/avr-test.hex"
+    ( cd examples/avr-example2 && make clean )
+
     ( cd examples/avr-cpp-example && make clean && make avr-test.hex )
     test_build_result "examples/avr-cpp-example/avr-test.hex"
+    ( cd examples/avr-cpp-example && make clean )
+
     ( cd examples/avr-cpp-example2 && make clean && make avr-c++test.hex )
     test_build_result "examples/avr-cpp-example2/avr-c++test.hex" 
+    ( cd examples/avr-cpp-example2 && make clean )
   ;;
 *)
   echo "architecture not supported $TARGETARCHITECTURE ! use one of arm-none-eabi, m68k-elf or avr"
